88bf296d0ec491e02a62fbccb6364913d20c6cb8
okapie <tatigreuze@yahoo.co.jp>
2019-01-04 08:31:37 +0900

    Create API: POST /api/v1/todo.
    Set ID column (auto increment).

--- server/main.go
+++ server/main.go
@@ -12,37 +12,58 @@ import (
 )
 
 type Todo struct {
+    Id      int    `json:"id"`
     Item    string `json:"item"`
 }
 
 type Todos []Todo
 
+func openDB() (db *sql.DB) {
+    dbDriver := "mysql"
+    dbUser := "root"
+    dbPassword := "root"
+    dbName := "db_todos"
+    db, err := sql.Open(dbDriver, dbUser+":"+dbPassword+"@/"+dbName)
+    if err != nil {
+        panic(err.Error())
+    }
+    return db
+}
+
 func getTodoList(w http.ResponseWriter, r *http.Request) {
-    db, err := sql.Open("mysql", "root:root@/db_todos")
+    db := openDB()
+    rows, err := db.Query("SELECT * FROM Tb_Todos ORDER BY id DESC")
     if err != nil {
         panic(err.Error())
     }
-
-    defer db.Close()
-
-    rows, err := db.Query("select item from tb_todos")
-    if err != nil {
-        panic(err.Error())
-    }
-
     var todos []Todo
-
     for rows.Next() {
+
+        var id int
         var item string
 
-        rows.Scan(&item)
-        todos = append(todos, Todo{ item })
+        rows.Scan(&id ,&item)
+        todos = append(todos, Todo{ id, item })
     }
-
     todosBytes, _ := json.Marshal(&todos)
 
     w.Write(todosBytes)
-    db.Close()
+    defer db.Close()
+}
+
+func postTodo(w http.ResponseWriter, r *http.Request) {
+    db := openDB()
+    if r.Method == "POST" {
+        item := r.FormValue("item")
+        insForm, err := db.Prepare("INSERT INTO Tb_Todos(item) VALUES(?)")
+        if err != nil {
+            panic(err.Error())
+        }
+        insForm.Exec(item)
+        log.Println("INSERT: Item: " + item)
+    }
+    defer db.Close()
+    http.Redirect(w, r, "/", 200)
 }
 
 func main() {
@@ -52,6 +73,7 @@ func main() {
 
     r := mux.NewRouter()
     r.HandleFunc("/api/v1/todos", getTodoList).Methods("GET")
+    r.HandleFunc("/api/v1/todo", postTodo).Methods("POST")
 
     log.Fatal(http.ListenAndServe(":8000", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)))
 }
