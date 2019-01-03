88882af4d8a4b274b06b1f0cd2ac855be2735e2e
okapie <tatigreuze@yahoo.co.jp>
2019-01-03 20:47:34 +0900

    Set MySQL Database for the API (GET /api/v1/todos).

--- server/main.go
+++ server/main.go
@@ -4,9 +4,11 @@ import (
     "encoding/json"
     "log"
     "net/http"
+    "database/sql"
 
     "github.com/gorilla/mux"
     "github.com/gorilla/handlers"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 type Todo struct {
@@ -16,13 +18,31 @@ type Todo struct {
 type Todos []Todo
 
 func getTodoList(w http.ResponseWriter, r *http.Request) {
-    todos := Todos{
-        Todo{Item: "Go to the office at 9 A.M."},
-        Todo{Item: "Go to the hospital at 1 P.M."},
-        Todo{Item: "Go shopping at 3 P.M."},
+    db, err := sql.Open("mysql", "root:root@/db_todos")
+    if err != nil {
+        panic(err.Error())
     }
 
-    json.NewEncoder(w).Encode(todos)
+    defer db.Close()
+
+    rows, err := db.Query("select item from tb_todos")
+    if err != nil {
+        panic(err.Error())
+    }
+
+    var todos []Todo
+
+    for rows.Next() {
+        var item string
+
+        rows.Scan(&item)
+        todos = append(todos, Todo{ item })
+    }
+
+    todosBytes, _ := json.Marshal(&todos)
+
+    w.Write(todosBytes)
+    db.Close()
 }
 
 func main() {
