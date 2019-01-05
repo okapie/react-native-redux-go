57a194a7feb4699089e69f575b5b7f9c4e4af778
okapie <tatigreuze@yahoo.co.jp>
2019-01-05 18:56:47 +0900

    Set flexbox for ItemForm component.

--- client/src/components/common/molecules/ItemForm/index.js
+++ client/src/components/common/molecules/ItemForm/index.js
@@ -4,7 +4,7 @@ import { InputComponent } from "../../atoms/Input";
 import { ButtonComponent } from "../../atoms/Button";
 
 export const ItemForm = ({ value, onPress }) => (
-  <View>
+  <View style={styles.container}>
     <InputComponent value={ value.item } />
     <ButtonComponent
       title="Delete"
@@ -15,6 +15,11 @@ export const ItemForm = ({ value, onPress }) => (
 );
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    marginBottom: 8
+  },
   input: {
     width: 200,
     height: 50,
